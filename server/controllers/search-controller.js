c69429eb45cfafdb9e1fd59d5021cfac8d331f8a
Yehor Zemzyulin <yehor.zemzyulin@gmail.com>
2022-10-29 00:47:50 +0300

    switched from pg to postgres module

--- server/controllers/search-controller.js
+++ server/controllers/search-controller.js
@@ -1,47 +1,41 @@
-import pool from '../models/database.js';
+import sql from '../models/database.js';
 import { capitalizeString, concatUnique } from '../utils/utils.js';
 
 // list 10 suggestions
 export async function find(req, res) {
-    try {
-        // query for start of city name match
+    let value1 = capitalizeString(req.query.search);
+    let value2 = req.query.search.toLowerCase();
 
-        let query1 = `SELECT * FROM cities WHERE name LIKE $1 ORDER BY name LIMIT 10`
-        let value1 = capitalizeString(req.query.search);
-        let result1 = await pool.query(query1, [`${value1}%`]);
-
-        // query for mid or end of city name match
-        let query2 = `SELECT * FROM cities WHERE name LIKE $1 OR name LIKE $2 ORDER BY name LIMIT 10`;
-        let value2 = req.query.search.toLowerCase();
-        let result2 = await pool.query(query2, [`%${value1}%`, `%${value2}%`]);
-        
-        // filter unique, take first 10
-        let results = concatUnique(result1.rows, result2.rows).splice(0, 10);
-
-        res.status(200).send(results);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+    // query for start of city name match
+    let query1 = await sql`SELECT * FROM cities 
+                            WHERE name LIKE ${value1 + '%'}
+                            ORDER BY name LIMIT 10`;
+    // query for mid or end of city name match
+    let query2 = await sql`SELECT * FROM cities
+                            WHERE name LIKE ${'%' + value1 + '%'} OR name LIKE ${'%' + value2 + '%'}
+                            ORDER BY name LIMIT 10`;
+    
+    // filter unique, take first 10
+    let result = concatUnique(query1, query2).splice(0, 10);
+    res.status(200).send(result);
 }
 
 // find all matches
 export async function findAll(req, res) {
-    try {
-        // query for exact match and start of city name match
-        let query1 = `SELECT * FROM cities WHERE name = $1 OR name = $2 OR name LIKE $2 ORDER BY name`;
-        let value1 = capitalizeString(req.query.search);
-        let result1 = await pool.query(query1, [`${req.query.search}`, `${value1}%`]);
+    let value0 = req.query.search;
+    let value1 = capitalizeString(req.query.search);
+    let value2 = req.query.search.toLowerCase();
 
-        // query for mid or end of city name match
-        let query2 = `SELECT * FROM cities WHERE name LIKE $1 OR name LIKE $2 ORDER BY name`;
-        let value2 = req.query.search.toLowerCase();
-        let result2 = await pool.query(query2, [`%${value2}%`, `%${value1}%`]);
+    // query for exact match and start of city name match
+    let query1 = await sql`SELECT * FROM cities
+                            WHERE name = ${value0} OR name = ${value1} OR name LIKE ${value1 + '%'}
+                            ORDER BY name`;
+    // query for mid or end of city name match
+    let query2 = await sql`SELECT * FROM cities
+                            WHERE name LIKE ${'%' + value2 + '%'} OR name LIKE ${'%' + value1 + '%'}
+                            ORDER BY name`;
 
-        // filter unique, take first 10
-        let results = concatUnique(result1.rows, result2.rows);
-        
-        res.status(200).send(results);
-    } catch (error) {
-        res.status(400).send(error);
-    }
+    // filter unique, take first 10
+    let result = concatUnique(query1, query2);
+    res.status(200).send(result);
 }
--- server/models/database.js
+++ server/models/database.js
@@ -1,32 +1,11 @@
-import pg from 'pg';
-const { Pool } = pg;
+import postgres from 'postgres';
 
-/*
-const pool = new Pool({
-  user: "mvc",
-  host: "localhost",
-  database: "cities_search",
-  password: "toomuch",
-  port: "5432"
-});*/
-
-const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+const sql = postgres({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT
-});
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD
+})
 
-
-/*
-
-DB_USER=mvc
-DB_PASSWORD=toomuch
-DB_HOST=localhost
-DB_POST=5432
-DB_DATABASE=cities_search
-PORT=8000
-*/
-
-export default pool;
+export default sql;
